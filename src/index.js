dba56a288fcebb18963759331226bf4386cc3d0d
ChengetaiS <162143946+ChengetaiS@users.noreply.github.com>
2025-09-23 16:39:47 +0200

    Added functionality using JS

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+function generateRecipe(event) {
+  event.preventDefault();
 
+  new Typewriter("#recipe-list", {
+    strings: "Hello World",
+    autoStart: true,
+    cursor: "✍",
+    delay: 100,
+  });
+}
+
+let recipeForm = document.querySelector("#recipe-generator-form");
+recipeForm.addEventListener("submit", generateRecipe);
